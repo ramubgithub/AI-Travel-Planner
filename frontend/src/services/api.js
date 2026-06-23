e0a2c3c16b9ef759dcca680626da7775b7762322
ramubgithub <ramubattula555@gmail.com>
2026-06-23 12:36:28 +0530

    Production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://ai-travel-planner-api.onrender.com/api"
 });
 
 api.interceptors.request.use(
